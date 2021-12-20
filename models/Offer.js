9dc6530202e57ad6d8d41196613d29c1a9441923
JeffreytheCoder <y2397371802@gmail.com>
2021-12-19 23:41:24 -0800

    offer model

---
+++ models/Offer.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const OfferSchema = new Schema({
+  type: {
+    type: String,
+    required: true,
+  },
+  course: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  seller: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  buyer: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  paid: {
+    type: Boolean,
+    default: false,
+  },
+  transferDeadline: {
+    type: Date,
+  },
+  expireDate: {
+    type: Date,
+  },
+  payDeadline: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model('offer', OfferSchema);
